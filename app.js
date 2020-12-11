7522fb1fa44837cff2ec78d73fd6f3ee1c7c05d9
Mohammad-sloiman <mohammad.soliman@utb.ecutbildning.se>
2020-12-11 12:19:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* Skriv din kod här */
 
-
+/* Mohammad Soliman */
 
 const url = `https://restcountries.eu/rest/v2/all`;
 
@@ -25,19 +25,19 @@ fetch(url).then(response => {
         landerna.push(land);
     }
     /* Här selecatar jag all img and h1 and h3 */
-    let img = document.querySelectorAll('img')
-    let landNamn = document.querySelectorAll('h1')
-    let timeLand = document.querySelectorAll('h3')
+    let img = document.querySelectorAll('img');
+    let landNamn = document.querySelectorAll('h1');
+    let timeLand = document.querySelectorAll('h3');
 
-    /* här loppar jag med de landerna i araay för att lägga till inehållet till html */
+    /* här loppar jag med de landerna i den araay för att lägga till inehållet till html */
     for (let i = 0; i < landerna.length; i++) {
 
         img[i].src = landerna[i].urlFlagga;
         landNamn[i].innerHTML = landerna[i].landetNamn;
-        timeLand[i].innerHTML = ` nuvarande tiden är klockan :<br> ${landerna[i].CurrentTime()}  : 00`;
+        timeLand[i].innerHTML = ` Den nuvarande tiden är :<br> ${landerna[i].CurrentTime()}`;
     }
 
-}).catch(
+}).catch(/* för att hanter erorr 404 */
     function (error) {
         console.log(error);
     }
@@ -55,15 +55,14 @@ function Land(flagga, namn, tid) {
 
 Land.prototype.CurrentTime = function () {
     let date = new Date; /*  för att kalla object Date */
-    let timHere = date.getHours(); /* för att hämta de nuvarande tid here i sverige */
-    let minuterHere = date.getMinutes() /* Har inte hunnit fixa */
-        let timeAsnumber = Number((this.tidZone).substr(3, 3));
-    console.log(minuterHere)
-
-    /* för att hämta hur många timmar extra har de i den andra landet alltså hur många utc plus eller minnus i den andra land */
-    let timForCountry = timHere + timeAsnumber;/* för att  addera timeAsnumber med den tiden vi har nu  */
+    let hoursHere = date.getUTCHours(); /* för att hämta de nuvarande tid here i hours i sverige */
+    let minuterHere = date.getMinutes() /*för att hämta tiden i minuter */
+    // console.log(minuterHere)
+    let tidZoneNumber = Number((this.tidZone).substr(3, 3));
+    /* för att hämta hur många tid Zonei den andra landet alltså hur många utc plus eller minnus i den andra land */
+    let timForCountry = `${hoursHere + tidZoneNumber} : ${minuterHere}`;/* för att  addera tidZoneNumber för den tiden vi har nu i  utc form */
     // console.log(timForCountry);
-    return timForCountry;
+    return timForCountry; /* Här gör vi return till värdet från methodet  */
 }
 
 
